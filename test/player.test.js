78567680c0404a525a81b2773ade2eae089e62b1
ophanim777 <rikyjo49@gmail.com>
2026-03-16 00:18:53 +0700

    add test to check if player can attack enemy board

--- test/player.test.js
+++ test/player.test.js
@@ -5,3 +5,14 @@ test("player has gameboard", () => {
 
   expect(player.gameboard).toBeDefined();
 });
+
+test("player can attack enemy board", () => {
+  const player1 = new Player();
+  const player2 = new Player();
+
+  player2.gameboard.placeShip(2, 1, 1);
+
+  const result = player1.attack(player2.gameboard, 1, 1);
+
+  expect(result).toBe("hit");
+});
