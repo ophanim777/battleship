d2f273964ccfbdca0e3ad91981ff66ad0c13bcbe
ophanim777 <rikyjo49@gmail.com>
2026-03-16 00:09:59 +0700

    add player type and game board for every player

--- src/player.js
+++ src/player.js
@@ -1,3 +1,8 @@
 import Gameboard from "./gameboard.js";
 
-export default class Player {}
+export default class Player {
+    constructor(type = "human") {
+  this.type = type;
+  this.gameboard = new Gameboard();
+}
+}
