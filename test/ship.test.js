f53992ca26c90ec3bf1f4ead68ad2936059406a9
ophanim777 <rikyjo49@gmail.com>
2026-03-15 23:22:37 +0700

    add hit test

--- test/ship.test.js
+++ test/ship.test.js
@@ -4,3 +4,9 @@ test("ship stores length", () => {
   const ship = new Ship(3);
   expect(ship.length).toBe(3);
 });
+
+test("ship records hit", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
