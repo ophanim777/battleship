eaa0060a0147981dd4f1a8b10509011c285fb30a
ophanim777 <rikyjo49@gmail.com>
2026-03-15 23:59:58 +0700

    add placing ship test

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,1 +1,8 @@
 import Gameboard from "../src/gameboard.js";
+
+test("place ship on board", () => {
+  const board = new Gameboard();
+  board.placeShip(3, 2, 3);
+
+  expect(board.ships.length).toBe(1);
+});
